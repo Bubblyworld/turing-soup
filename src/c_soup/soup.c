21f0647430d1e7298e14b88cd50b93fc0643d999
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2022-12-22 09:59:43 +0200

    Midway through the C rewrite, currently redex parsing is fast, clean and memory-leak free.

--- src/c_soup/soup.c
+++ src/c_soup/soup.c
@@ -1,254 +1,40 @@
 #include "soup.h"
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
 
-// Converts a string literal into a (dynamically allocated) term. The caller
-// is responsible for freeing the term.
-term_t term_new(const char *str) {
-	return strdup(str);
+// Normalises the given term by removing all unnecessary parentheses. Note that
+// two terms can have different normal forms, and yet still behave in exactly
+// the same way: S(KI)Ix = KIx(Ix) = I(Ix) = Ix, for instance.
+void normalise(term_t term) {
 }
 
-// Initialises a subterms_t.
-void subterms_init(subterms_t *subterms) {
-	subterms->size = 0;
-}
-
-// Removes any unnecessary brackets in the given term.
-// TODO: there's some kind of bug here, IS((SI)S) -> IS(SI)S instead of IS(SIS)
-void term_normalise(term_t term) {
-	int i = 0; // writing index
-	int count = 0; // bracket level
-	int first_char = 1; // 0 if first char in new bracket level, 1 otherwise
-	for (int j = 0; j < strlen(term); j++) {
-		if (term[j] == '(') {
-			if (first_char) {
-				count++;
-				continue;
-			} else {
-				// If the bracket contains a single character, we skip brackets:
-				if (term[j+2] == ')') {
-					term[i] = term[j+1];
-					i++;
-					j += 2;
-					first_char = 0;
-					continue;
-				}
-
-				term[i] = term[j];
-				first_char = 1;
-				count++;
-				i++;
-				continue;
-			}
-		} else if (term[j] == ')') {
-			if (count == 0) {
-				continue;
-			} else {
-				term[i] = term[j];
-				count--;
-				i++;
-				continue;
-			}
-		}
-
-		// We need to write this char:
-		term[i] = term[j];
-		first_char = 0;
-		i++;
-	}
-	term[i]= '\0';
-}
-
-// Pushes an index to the end of a subterms_t.
-void subterms_push(subterms_t *subterms, int index) {
-	if (subterms->size >= 6) {
-		subterms->indices[5] = index;
-	} else {
-		subterms->indices[subterms->size] = index;
-		subterms->size++;
-	}
-}
-
-// Copies a subterm from a term to another string.
-void subterms_copy(subterms_t *subterms, term_t dest, term_t src, int index) {
-	int start = subterms->indices[index];
-	int end = subterms->indices[index + 1];
-	strncpy(dest, src + start, end - start);
-}
-
-// Initialises a subterms list. It is assumed that the passed subterms list is
-// uninitialised, and it is the caller's responsibility to free the list when
-// it is no longer needed.
-void subterms_list_init(subterms_list_t *list) {
-	list->size = 0;
-	list->capacity = 128;
-	list->subterms = malloc(128 * sizeof(subterms_t));
-}
-
-// Frees a subterms list. The subterms list should not be used after this function is
-// called, and it is the caller's responsibility to ensure that the list is
-// initialised before it is used again.
-void subterms_list_free(subterms_list_t *list) {
-	list->size = 0;
-	list->capacity = 0;
-	free(list->subterms);
-}
-
-// Resizes a subterms list to the given capacity. The subterms list should be
-// initialised before this function is called.
-void subterms_list_resize(subterms_list_t *list, int capacity) {
-	assert(list->capacity <= capacity);
-	list->capacity = capacity;
-	list->subterms = realloc(list->subterms, capacity * sizeof(subterms_t));
-}
-
-// Pushes a newly initialised subterms_t onto the end of the list.
-void subterms_list_push(subterms_list_t *list) {
-	if (list->size == list->capacity) {
-		subterms_list_resize(list, list->capacity * 2);
-	}
-	list->size++;
-	list->subterms[list->size - 1].size = 0;
-}
-
-// Returns the topmost subterm_t in the given list.
-subterms_t *subterms_list_top(subterms_list_t *list) {
-	assert(list->size > 0);
-	return &list->subterms[list->size - 1];
-}
-
-// Lists all the redexes in a term. The subterms lists should be initialised
-// before this function is called.
-// TODO: we should make sure we never allow empty subterms
-void list_redexes(term_t term, subterms_list_t *stack, subterms_list_t *results) {
-	stack->size = 0;
-	results->size = 0;
-	if (term[0] == '\0') {
-		return;
+// Writes all of the redexes in the given term to s->redexes, and returns
+// true if any redexes were found.
+bool redexes(term_t term, term_subterms_t *s) {
+	term_subterms_t_reset(s);	
+	if (term == NULL || term[0] == '\0') {
+		return false;
 	}
 
-	// Our strategy is to walk the term, pushing redexes onto the stack as we
-	// encounter open brackets '('. When we encounter a close bracket ')', we
-	// pop the top redex off the stack, and add the index to the previous redex.
-	// If the popped redex is valid, we add it to the results list.
 	int i = -1;
-	subterms_list_push(stack);
-	do { // hack so we handle '\0' explicitly
+	subterms_t_push(s->subterms);
+	do { // hack so we explicitly handle the '\0' char
 		i++;
 		if (term[i] == '(') {
-			subterms_push(subterms_list_top(stack), i);
-			subterms_list_push(stack);
+			indices_t_push(subterms_t_top(s->subterms));
+			*indices_t_top(subterms_t_top(s->subterms)) = i;
+			subterms_t_push(s->subterms);
 		} else if (term[i] == ')' || term[i] == '\0') {
-			subterms_push(subterms_list_top(stack), i);
-			subterms_t *subterm = subterms_list_top(stack);
-			stack->size--;
-			if (subterm->size > 0) {
-				if ((subterm->size >= 3 && term[subterm->indices[0]] == 'I') ||
-					(subterm->size >= 4 && term[subterm->indices[0]] == 'K') ||
-					(subterm->size >= 5 && term[subterm->indices[0]] == 'S')) {
-					subterms_list_push(results);
-					memcpy(subterms_list_top(results), subterm, sizeof(subterms_t));
-				}
+			indices_t_push(subterms_t_top(s->subterms));
+			*indices_t_top(subterms_t_top(s->subterms)) = i;
+			indices_t *pop = subterms_t_pop(s->subterms);
+			if (subterm_is_redex(term, pop)) {
+				subterms_t_push(s->redexes);
+				indices_t_copy(pop, subterms_t_top(s->redexes));
 			}
 		} else {
-			subterms_push(subterms_list_top(stack), i);
+			indices_t_push(subterms_t_top(s->subterms));
+			*indices_t_top(subterms_t_top(s->subterms)) = i;
 		}
 	} while (term[i] != '\0');
-}
 
-// Applies a valid redex to a term, i.e. evaluates one of the combinators in
-// the term. The term is modified in place, reallocating memory if necessary.
-int apply_redex(term_t term, subterms_t *redex) {
-	// We support three kinds of combinators, each with different arity:
-	// 1. K: K x y = x
-	// 2. S: S x y z = x z (y z)
-	// 3. I: I x = x
-	if (redex->size < 2) {
-		return 0;
-	}
-
-	// If the first subterm is not a single character, and not equal to any of
-	// the combinators, we can't apply the redex:
-	if (redex->indices[0] + 1 != redex->indices[1]) {
-		if (term[redex->indices[0]] != 'K' &&
-			term[redex->indices[0]] != 'S' &&
-			term[redex->indices[0]] != 'I') {
-			return 0;
-		}
-	}
-	
-	// The size of the redex is the number of subterms it contains, plus one
-	// for the end of the term. This tells us the arity of the redex:
-	int reduced = 0;
-	int arity = redex->size - 1;
-	if (term[redex->indices[0]] == 'K' && arity >= 3) {
-		char *subterm = term + redex->indices[0];
-		char *x = term + redex->indices[1];
-		int x_len = redex->indices[2] - redex->indices[1];
-		memmove(subterm, x, x_len);
-		char *rest = term + redex->indices[3];
-		memmove(subterm + x_len, rest, strlen(rest)+1);
-		reduced = 1;
-	} else if (term[redex->indices[0]] == 'S' && arity >= 4) {
-		char *x = term + redex->indices[1];		
-		int x_len = redex->indices[2] - redex->indices[1];
-		char *y = term + redex->indices[2];		
-		int y_len = redex->indices[3] - redex->indices[2];
-		char *z = term + redex->indices[3];		
-		int z_len = redex->indices[4] - redex->indices[3];
-		term = realloc(term, sizeof(char) * (strlen(term) + z_len + 2 + 1));
-		char *rest = term + redex->indices[4];
-		memmove(rest + z_len + 2, rest, strlen(rest)+1);
-		memmove(x + x_len + y_len + z_len + 1, z, z_len);
-		*(x + x_len + y_len + 2*z_len + 1) = ')';
-		memmove(x + x_len + z_len + 1, y, y_len);
-		*(x + x_len + z_len) = '(';
-		memmove(x + x_len, x + x_len + y_len + z_len + 1, z_len);
-		memmove(term + redex->indices[0], x, strlen(x)+1);
-		reduced = 1;
-	} else if (term[redex->indices[0]] == 'I' && arity >= 2) {
-		char *subterm = term + redex->indices[0];
-		char *x = term + redex->indices[1];
-		int x_len = redex->indices[2] - redex->indices[1];
-		memmove(subterm, x, x_len);
-		char *rest = term + redex->indices[2];
-		memmove(subterm + x_len, rest, strlen(rest)+1);
-		reduced = 1;
-	}
-
-	if (reduced) {
-		term_normalise(term);
-	}
-	return reduced;
-}
-
-int main() {
-	term_t term = term_new("IS((SI)S)");
-	printf("%s ->", term);
-	term_normalise(term);
-	printf("%s\n", term);
-	return 0;
-
-	//term_t term = term_new("SII(SI)SKS(KKI)");
-	//subterms_list_t stack;
-	//subterms_list_t results;
-	//subterms_list_init(&stack);
-	//subterms_list_init(&results);
-
-	//// Calculate how many times we can call "list_redexes" in 1 second:
-	//int i = 0;
-	//clock_t start = clock();
-	//do {
-	//	list_redexes(term, &stack, &results);
-	//	if (results.size == 0) {
-	//		break;
-	//	}
-	//	apply_redex(term, &results.subterms[0]);
-	//	printf("%s\n", term);
-	//	i++;
-	//} while (clock() - start < CLOCKS_PER_SEC);
-	//printf("list_redexes: %d calls per second\n", i);
+	return s->redexes->size > 0;
 }
--- src/c_soup/soup.h
+++ src/c_soup/soup.h
@@ -1,49 +1,143 @@
 #pragma once
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-// Combinator terms are simply C strings:
+/******************************************************************************
+ *                   UTILITY FUNCTIONS AND DATA STRUCTURES                    *
+ ******************************************************************************/
+
+// I get annoyed by C's lack of boolean types, so I'm hiding it:
+typedef int bool;
+#define true 1
+#define false 0
+
+// A simple stack data structure that resizes itself as necessary. Useful as
+// cached working memory for term manipulation to avoid allocations.
+#define STACK(type, name) \
+  typedef struct { \
+    type *data; \
+    int size; \
+    int capacity; \
+  } name; \
+  inline static void name##_init(name *s) { \
+    s->data = malloc(sizeof(type) * 16); \
+    s->size = 0; \
+    s->capacity = 16; \
+    for (int i = 0; i < s->capacity; i++) { \
+      type##_init(&s->data[i]); \
+    } \
+  } \
+  inline static void name##_reset(name *s) { \
+    s->size = 0; \
+  } \
+  inline static void name##_free(name *s) { \
+    for (int i = 0; i < s->capacity; i++) { \
+      type##_free(&s->data[i]); \
+    } \
+    free(s->data); \
+  } \
+  inline static void name##_push(name *s) { \
+    if (s->size == s->capacity) { \
+      int new_capacity = s->capacity ? s->capacity * 2 : 1; \
+      s->data = (type*) realloc(s->data, new_capacity * sizeof(type)); \
+      for (int i = s->capacity; i < new_capacity; i++) { \
+        type##_init(&s->data[i]); \
+      } \
+      s->capacity = new_capacity; \
+    } \
+    s->size++; \
+    type##_reset(&s->data[s->size - 1]); \
+  } \
+  inline static void name##_copy(name *src, name *dst) { \
+    name##_reset(dst); \
+    for (int i = 0; i < src->size; i++) { \
+      name##_push(dst); \
+      type##_copy(&src->data[i], &dst->data[i]); \
+    } \
+  } \
+  inline static type *name##_pop(name *s) { \
+    return &s->data[--s->size]; \
+  } \
+  inline static type *name##_top(name *s) { \
+    return &s->data[s->size - 1]; \
+  }
+
+/******************************************************************************
+ *                            LOW-LEVEL SOUP API                              *
+ ******************************************************************************/
+
+// Terms are strings of combinators, variables and parentheses. Terms are bound
+// by left-associativity, so the term "ab(c)" is equivalent to "(ab)c", with
+// parentheses being used to override this. We represent terms internally as
+// C strings for efficiency.
 typedef char *term_t;
+inline static term_t term_t_new(const char *str) {
+  return strdup(str);
+}
+inline static void term_t_free(term_t term) {
+  free(term);
+}
+void normalise(term_t term); // IMPLEMENT ME
 
-// Functions for working with terms:
-term_t term_new(const char *str);
-void term_normalise(term_t term);
+// When working with terms, we often need to store the set of indices of
+// successive subterms within the term, so that we can easily compute
+// combinator reductions. We represent these indices as a stack of integers.
+// TODO: these int_XXX methods can probably be handled by the macro, and it
+//       might even be faster to do that anyway.
+inline static void int_init(int *i) { *i = 0; } // for STACK
+inline static void int_reset(int *i) { *i = 0; } // for STACK
+inline static void int_free(int *i) {} // for STACK
+inline static void int_copy(int *src, int *dst) { *dst = *src; } // for STACK
+STACK(int, indices_t);
+inline term_t subterm(term_t term, indices_t *indices) {
+  int start = indices->data[0];
+  int end = indices->data[indices->size - 1];
+  char *subterm = malloc(end - start + 1);
+  memcpy(subterm, term + start, end - start);
+  subterm[end - start] = '\0';
+  return subterm;
+}
+inline static int subterm_length(term_t term, indices_t *indices, int index) {
+  int start = indices->data[index];
+  int end = index == indices->size - 1 ? strlen(term) : indices->data[index + 1];
+  return end - start;
+}
+inline static bool subterm_is_redex(term_t term, indices_t *indices) {
+  return indices->size > 1 && subterm_length(term, indices, 0) == 1 && (
+    (term[indices->data[0]] == 'S' && indices->size >= 5) || 
+    (term[indices->data[0]] == 'K' && indices->size >= 4) ||
+    (term[indices->data[0]] == 'I' && indices->size >= 3)
+  );
+}
 
-// Subterms keeps track of the indices of successive subterms in a term. These
-// are used to check for redexes and for fast evaluation.
+// The first useful thing we can do with terms is calculate their sets of
+// subterms, as well as their sets of redexes. To make this really fast we
+// keep a cached pair of stacks as working memory for the algorithms:
+STACK(indices_t, subterms_t);
 typedef struct {
-	int size;       // The number of set indices.
-	int indices[6]; // We never need more than 6 subterms for redexes.
-} subterms_t;
-
-// Functions for working with subterms:
-void subterms_init(subterms_t *subterms);
-void subterms_push(subterms_t *subterms, int index);
-void subterms_copy(subterms_t *subterms, term_t dest, term_t src, int index);
+  subterms_t *subterms;
+  subterms_t *redexes;
+} term_subterms_t;
+inline static void term_subterms_t_init(term_subterms_t *t) {
+  t->subterms = malloc(sizeof(subterms_t));
+  t->redexes = malloc(sizeof(subterms_t));
+  subterms_t_init(t->subterms);
+  subterms_t_init(t->redexes);
+}
+inline static void term_subterms_t_free(term_subterms_t *t) {
+  subterms_t_free(t->subterms);
+  subterms_t_free(t->redexes);
+}
+inline static void term_subterms_t_reset(term_subterms_t *t) {
+  subterms_t_reset(t->subterms);
+  subterms_t_reset(t->redexes);
+}
+// Constraint: must return true if there were any redexes, false otherwise.
+bool redexes(term_t term, term_subterms_t *s); // IMPLEMENT ME
 
 /******************************************************************************
- *                       LISTING REDEXES IN A TERM														*
- * Our general philosophy in this implementation is to avoid allocations as	  *
- * much as possible. Thus for listing redexes in a term, the user passes in	  *
- * input/output structures that are reused for each call, and reallocated as  *
- * necessary if they are too small. This is a bit more complicated than		    *
- * simply returning a vector of redexes, but it's much more efficient.			  *
+ *                       EVALUATING A REDEX IN A TERM                          *
  ******************************************************************************/
 
-typedef struct {
-	int size;            // The number of subterms_t currently in the list.
-	int capacity;        // The number of subterms_t that can be stored.
-	subterms_t *subterms; // The list of subterms_t.
-} subterms_list_t;
-
-// Functions for working with subterms_list__t:
-void subterms_list_init(subterms_list_t *list);
-void subterms_list_free(subterms_list_t *list);
-void subterms_list_resize(subterms_list_t *list, int capacity);
-void subterms_list_push(subterms_list_t *list);
-subterms_t *subterms_list_top(subterms_list_t *list);
-void list_redexes(term_t term, subterms_list_t *stack, subterms_list_t *results);
-
-/******************************************************************************
- *                       EVALUATING A REDEX IN A TERM													*
- ******************************************************************************/
-
-int apply_redex(term_t term, subterms_t *redex);
+//int apply_redex(term_t term, subterms_t *redex);
